0849c49fb340926f6ac3894631ab63a50885c425
Aadhithya Sankar <a.sankar@tum.de>
2022-05-08 23:39:24 +0200

    fix: add version info

--- rajinipp/__init__.py
+++ rajinipp/__init__.py
@@ -1,3 +1,8 @@
 """rajini++"""
 
 __version__ = "0.0.1"
+__version_str__ = (
+    f"rajini++ v{__version__}."
+    + "\nrajini++(rajinipp) is a programming language based on the iconic dialogues of super star Rajinikanth."
+    + "\nCreated by Aadhithya Sankar(a.sankar@tum.de)."
+)
