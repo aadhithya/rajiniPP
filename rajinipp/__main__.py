da1bcfd105414e82574803eabf68ee1bfbbb6cf6
Aadhithya Sankar <a.sankar@tum.de>
2022-05-08 22:08:21 +0200

    fix(__main__.py): make program work

--- rajinipp/__main__.py
+++ rajinipp/__main__.py
@@ -1,19 +1,33 @@
 import os
+import pdb
 from pathlib import Path
 
 from loguru import logger
 
 from .lexer import Lexer
+from .parser import Parser
+from .utils import read_yml
 
 if __name__ == "__main__":
-    input_text = "print(4+2);"
+    with open("./test-programs/hello.rpp", "r") as f:
+        input_text = f.read()
 
     yml_path = os.path.join(Path(__file__).parent, "token.yml")
     logger.info(yml_path)
 
-    lexer = Lexer(yml_path).get_lexer()
+    tokens_dict = read_yml(yml_path)
 
+    lexer = Lexer(tokens_dict).get_lexer()
     tokens = lexer.lex(input_text)
 
-    for token in tokens:
-        print(token)
+    # for token in tokens:
+    #     print(token)
+
+    parser = Parser(list(tokens_dict.keys()))
+    parser.parse()
+
+    parser = parser.get_parser()
+
+    parsed = parser.parse(tokens)
+
+    parsed.eval()
