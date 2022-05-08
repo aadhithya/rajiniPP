eff7808a7a8304ca0ea3b1ca2541fb8f83ee5070
Aadhithya Sankar <a.sankar@tum.de>
2022-05-08 23:39:51 +0200

    fix(__main__): add more commands for the app

--- rajinipp/__main__.py
+++ rajinipp/__main__.py
@@ -2,9 +2,10 @@ import os
 import sys
 from pathlib import Path
 
-import typer
 from loguru import logger
+from typer import Typer
 
+from . import __version_str__
 from .lexer import Lexer
 from .parser import Parser
 from .utils import read_yml
@@ -12,21 +13,41 @@ from .utils import read_yml
 logger.remove()
 logger.add(sys.stderr, level="INFO")
 
+app = Typer()
 
-def main(file_path: str):
+
+@app.command()
+def version():
+    print(__version_str__)
+
+
+@app.command()
+def tokenize(file_path: str):
     with open(file_path, "r") as f:
         input_text = f.read()
 
     yml_path = os.path.join(Path(__file__).parent, "token.yml")
-    # logger.info(yml_path)
 
     tokens_dict = read_yml(yml_path)
 
     lexer = Lexer(tokens_dict).get_lexer()
     tokens = lexer.lex(input_text)
 
-    # for token in tokens:
-    #     print(token)
+    for token in tokens:
+        print(token)
+
+
+@app.command()
+def run(file_path: str):
+    with open(file_path, "r") as f:
+        input_text = f.read()
+
+    yml_path = os.path.join(Path(__file__).parent, "token.yml")
+
+    tokens_dict = read_yml(yml_path)
+
+    lexer = Lexer(tokens_dict).get_lexer()
+    tokens = lexer.lex(input_text)
 
     parser = Parser(list(tokens_dict.keys()))
     parser.parse()
@@ -39,4 +60,4 @@ def main(file_path: str):
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
