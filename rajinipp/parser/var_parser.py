5bd47c4d30c730001fafa605acd6db0a9345108a
Aadhithya Sankar <a.sankar@tum.de>
2022-05-10 22:15:07 +0200

    feat(VariableParser): add support to assign value and to declare variables

--- rajinipp/parser/var_parser.py
+++ rajinipp/parser/var_parser.py
@@ -1,6 +1,6 @@
 from loguru import logger
 
-from ..ast.base import Assignment
+from ..ast.base import VarAssign, VarDeclare
 
 
 class VariableParser:
@@ -8,11 +8,18 @@ class VariableParser:
 
     def parse(self, pg):
         @pg.production(
-            "statement : START_ASSIGN variable ASSIGN expression SEMI_COLON"
+            "statement : START_DECLARE variable DECLARE expression SEMI_COLON"
         )
         @pg.production(
-            "statement : START_ASSIGN variable EQUALS expression SEMI_COLON"
+            "statement : START_DECLARE variable DECLARE_ALT expression SEMI_COLON"
         )
-        def assign_stmt(p):
+        def declare_var_expr(p):
             logger.debug("VariableParser --> assign_stmt")
-            return Assignment(p[1], p[3])
+            return VarDeclare(p[1], p[3])
+
+        @pg.production("statement : variable ASSIGN expression SEMI_COLON")
+        @pg.production(
+            "statement : variable ASSIGN_ALT expression SEMI_COLON"
+        )
+        def assign_var_expr(p):
+            return VarAssign(p[0], p[2])
