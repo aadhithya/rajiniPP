2d0176ca78e4253c4804681f93cfbdbfd5f20561
Aadhithya Sankar <a.sankar@tum.de>
2022-05-12 00:15:17 +0200

    refactor(ExpressionParser): drastically simplify expression parser

--- rajinipp/parser/expr_parser.py
+++ rajinipp/parser/expr_parser.py
@@ -1,33 +1,26 @@
 from loguru import logger
 
-from ..ast.ops import Div, Mod, Mul, Sub, Sum, UnarySub, UnarySum
+from ..ast.ops import UnarySub, UnarySum
 
 
 class ExpressionParser:
     """parse all kinds of expressions."""
 
     def parse(self, pg):
-        @pg.production("expression : expression SUM expression")
-        @pg.production("expression : expression SUB expression")
-        @pg.production("expression : expression MUL expression")
-        @pg.production("expression : expression DIV expression")
-        @pg.production("expression : expression MOD expression")
+        @pg.production(
+            "expression : expression mathop expression", precedence="MATHOP"
+        )
+        @pg.production(
+            "expression : expression logicalop expression",
+            precedence="LOGICOP",
+        )
         def binary_expr(p):
             logger.debug("Parser --> expression")
             left = p[0]
             right = p[2]
-            op = p[1]
+            OpNode = p[1]
 
-            if op.gettokentype() == "SUM":
-                return Sum(left, right)
-            if op.gettokentype() == "SUB":
-                return Sub(left, right)
-            if op.gettokentype() == "MUL":
-                return Mul(left, right)
-            if op.gettokentype() == "DIV":
-                return Div(left, right)
-            if op.gettokentype() == "MOD":
-                return Mod(left, right)
+            return OpNode(left, right)
 
         @pg.production("expression : SUB expression")
         @pg.production("expression : SUM expression")
