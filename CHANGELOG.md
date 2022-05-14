f27c4dfa7bc46e9256b0766226d5115d82a176f2
semantic-release-action <action@github.com>
2022-05-14 21:09:40 +0000

    Bump to 0.1.0
    Bump to 0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+<!--next-version-placeholder-->
+
+## v0.1.0 (2022-05-14)
+### Feature
+* **api.py:** Add api ([`f559d12`](https://github.com/aadhithya/rajiniPP/commit/f559d122bc3abb0616457bdb85ec1c5ef8451036))
--- rajinipp/__init__.py
+++ rajinipp/__init__.py
@@ -1,6 +1,6 @@
 """rajini++"""
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __version_str__ = (
     f"rajini++ v{__version__}."
     + "\nrajini++(rajinipp) is a hobby programming language based on the iconic dialogues of super star Rajinikanth."
