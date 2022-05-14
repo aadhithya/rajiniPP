d6e5eb37b8cce82e4b80762480d0ab8284882a76
Aadhithya Sankar <a.sankar@tum.de>
2022-05-14 14:55:50 +0200

    feat(api.py): add api to execute rajini++ code from python scripts

---
+++ rajinipp/api.py
@@ -0,0 +1,36 @@
+"""
+# rajinipp python API.
+The rejinipp api exposes the rajinipp interpreter
+to python scripts.
+"""
+
+import os
+import sys
+from pathlib import Path
+
+from loguru import logger
+
+from .lexer import Lexer
+from .parser import Parser
+from .utils import read_yml
+
+
+def exec(code: str):
+    logger.remove()
+    logger.add(sys.stderr, level="ERROR")
+
+    yml_path = os.path.join(Path(__file__).parent, "token.yml")
+
+    tokens_dict = read_yml(yml_path)
+
+    lexer = Lexer(tokens_dict).get_lexer()
+    tokens = lexer.lex(code)
+
+    parser = Parser(list(tokens_dict.keys()))
+    parser.parse()
+
+    parser = parser.get_parser()
+
+    parsed = parser.parse(tokens)
+
+    parsed.eval()
