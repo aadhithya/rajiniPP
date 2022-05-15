abb571620505710dbd6403b8c677c1f0053b23a4
Aadhithya Sankar <a.sankar@tum.de>
2022-05-16 00:31:26 +0200

    test(test_flow_control.py): add test for for-loops

--- tests/test_flow_control.py
+++ tests/test_flow_control.py
@@ -11,3 +11,9 @@ def test_if_else(if_else_code, capsys):
     runner.exec(if_else_code)
     out, err = capsys.readouterr()
     assert "x ( 5.0 ) is less than 10!" in out.strip()
+
+
+def test_for_loop(for_loop_code, capsys):
+    runner.exec(for_loop_code)
+    out, err = capsys.readouterr()
+    assert "After loop: X = 14.0" in out.strip()
