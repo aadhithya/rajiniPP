42a99ab504a10302ca3badc21a9baa889bc56ee5
Aadhithya Sankar <a.sankar@tum.de>
2022-05-15 12:56:38 +0200

    refactor(__main__.py): remove code dupolication and unused imports

--- rajinipp/__main__.py
+++ rajinipp/__main__.py
@@ -1,16 +1,10 @@
-import os
-import sys
-from pathlib import Path
-
-from loguru import logger
 from typer import Typer
 
 from . import __version_str__
-from .lexer import Lexer
-from .parser.parser import Parser
-from .utils import read_yml
+from .runner import RppRunner
 
 app = Typer()
+runner = RppRunner()
 
 
 @app.command()
@@ -23,15 +17,8 @@ def version():
 def tokenize(file_path: str):
     """prints the tokens from the program."""
     with open(file_path, "r") as f:
-        input_text = f.read()
-
-    yml_path = os.path.join(Path(__file__).parent, "token.yml")
-
-    tokens_dict = read_yml(yml_path)
-
-    lexer = Lexer(tokens_dict).get_lexer()
-    tokens = lexer.lex(input_text)
-
+        code_str = f.read()
+    tokens = runner.tokenize(code=code_str)
     for token in tokens:
         print(token)
 
@@ -39,29 +26,10 @@ def tokenize(file_path: str):
 @app.command()
 def run(file_path: str, debug: bool = False):
     """executes a rajini++ program."""
-
     level = "DEBUG" if debug else "INFO"
-    logger.remove()
-    logger.add(sys.stderr, level=level)
-
     with open(file_path, "r") as f:
-        input_text = f.read()
-
-    yml_path = os.path.join(Path(__file__).parent, "token.yml")
-
-    tokens_dict = read_yml(yml_path)
-
-    lexer = Lexer(tokens_dict).get_lexer()
-    tokens = lexer.lex(input_text)
-
-    parser = Parser(list(tokens_dict.keys()))
-    parser.parse()
-
-    parser = parser.get_parser()
-
-    parsed = parser.parse(tokens)
-
-    parsed.eval()
+        code_str = f.read()
+    runner.exec(code=code_str, log_level=level)
 
 
 @app.callback()
