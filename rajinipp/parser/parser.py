24263bcf3a10514dbe4b02197e870885fa740f72
Aadhithya Sankar <a.sankar@tum.de>
2022-05-15 15:42:55 +0200

    feat(parser.py): add line parser to parse statements, refactor code

--- rajinipp/parser/parser.py
+++ rajinipp/parser/parser.py
@@ -1,8 +1,10 @@
+from abc import abstractclassmethod
 from typing import List
 
 from loguru import logger
 from rply import ParserGenerator
 
+from ..ast.base import Expression, Print
 from ..ast.blocks import MainBlock
 from .atom_parser import AtomParser
 from .cond_parser import ConditionalParser
@@ -13,7 +15,7 @@ from .stmt_parser import StatementParser
 from .var_parser import VariableParser
 
 
-class Parser:
+class ParserBase:
     def __init__(self, tokens: List[str]) -> None:
         self.pg = ParserGenerator(
             tokens=tokens,
@@ -38,11 +40,10 @@ class Parser:
         ]
 
     def parse(self):
-        @self.pg.production("main : PGM_START statements PGM_END")
-        def main(p):
-            logger.debug("Parser --> main")
-            return MainBlock(p[1])
+        pass
 
+    def init_parsers(self):
+        """initialize other parsers"""
         # * Add all other parsers.
         for parser in self.parsers:
             parser.parse(self.pg)
@@ -52,4 +53,35 @@ class Parser:
             raise ValueError(token)
 
     def get_parser(self):
+        """builds and returns parser"""
         return self.pg.build()
+
+
+class ProgramParser(ParserBase):
+    def parse(self):
+        @self.pg.production("main : PGM_START statements PGM_END")
+        def main(p):
+            logger.debug("Parser --> main")
+            return MainBlock(p[1])
+
+        self.init_parsers()
+
+
+class LineParser(ParserBase):
+    def __init__(self, tokens: List[str]) -> None:
+        super().__init__(tokens)
+        # * remove StatementParser. We will build our own here.
+        self.parsers.pop(0)
+        # * remove ConditionalParser. Line Parser will not support it yet.
+        self.parsers.pop(3)
+
+    def parse(self):
+        @self.pg.production("statement : PRINT printexprs SEMI_COLON")
+        def print_stmt(p):
+            return Print(p[1])
+
+        @self.pg.production("statement : expression SEMI_COLON")
+        def expr_stms(p):
+            return Expression(p[0])
+
+        self.init_parsers()
