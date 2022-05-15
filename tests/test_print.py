4b90346724ab4ec6b3cb79657ebd7f6dbb4de030
Aadhithya Sankar <a.sankar@tum.de>
2022-05-15 12:57:08 +0200

    test(tests): update tests after project refactor

--- tests/test_print.py
+++ tests/test_print.py
@@ -1,14 +1,13 @@
-import pytest
-from rajinipp import api
+from . import runner
 
 
 def test_print(hello_world, capsys):
-    api.exec(hello_world)
+    runner.exec(hello_world)
     out, err = capsys.readouterr()
     assert out.strip() == "Hello, World!"
 
 
 def test_multi_print(multi_print, capsys):
-    api.exec(multi_print)
+    runner.exec(multi_print)
     out, err = capsys.readouterr()
     assert out.strip() == "5 + 5 = 10.0"
