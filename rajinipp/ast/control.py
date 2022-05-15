7a2e25bcf8112a59c61aa4b40fbdd300238ff0d3
Aadhithya Sankar <a.sankar@tum.de>
2022-05-15 12:28:04 +0200

    feat(IfElseCondition): add node to interpret if-else statements

--- rajinipp/ast/control.py
+++ rajinipp/ast/control.py
@@ -8,6 +8,20 @@ class IfCondition(Node):
         self.value = value
 
     def eval(self):
-        if self.condition:
+        if self.condition.eval():
             return self.value.eval()
         return
+
+
+class IfElseCondition(Node):
+    def __init__(self, condition, true_value, false_value) -> None:
+        super().__init__()
+        self.condition = condition
+        self.true_value = true_value
+        self.false_value = false_value
+
+    def eval(self):
+        if self.condition.eval():
+            return self.true_value.eval()
+        else:
+            return self.false_value.eval()
