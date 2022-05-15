a0000b34568960bc738dcd5ced6c9a1f98961817
Aadhithya Sankar <a.sankar@tum.de>
2022-05-16 00:28:03 +0200

    fix(__main__.py): fix logger levels

--- rajinipp/__main__.py
+++ rajinipp/__main__.py
@@ -1,12 +1,12 @@
-import pdb
+import sys
 
+from loguru import logger
 from typer import Typer
 
 from . import __version__, __version_str__
 from .runner import RppRunner
 
 app = Typer()
-runner = RppRunner()
 
 
 @app.command()
@@ -18,8 +18,13 @@ def version():
 @app.command()
 def tokenize(file_path: str):
     """prints the tokens from the program."""
+    logger.remove()
+    logger.add(sys.stderr, level="ERROR")
+
     with open(file_path, "r") as f:
         code_str = f.read()
+
+    runner = RppRunner()
     tokens = runner.tokenize(code=code_str)
     for token in tokens:
         print(token)
@@ -28,15 +33,21 @@ def tokenize(file_path: str):
 @app.command()
 def run(file_path: str, debug: bool = False):
     """executes a rajini++ program."""
-    level = "DEBUG" if debug else "INFO"
+    level = "DEBUG" if debug else "ERROR"
+    logger.remove()
+    logger.add(sys.stderr, level=level)
+
     with open(file_path, "r") as f:
         code_str = f.read()
+
+    runner = RppRunner()
     runner.exec(code=code_str, log_level=level)
 
 
 @app.command()
 def shell():
-    print(f"rajini++ v{'0.2.0'}")
+    runner = RppRunner()
+    print(f"rajini++ v{__version__}")
     while True:
         code_line = input("rajinipp>> ")
         output = runner.eval(code_line)
