0cb870facc5d0dfb22d9f72b5f0a30e6e36d1850
Aadhithya Sankar <a.sankar@tum.de>
2022-05-15 15:43:31 +0200

    feat(RppRunner): add eval function to evaluate statements, refactor code

--- rajinipp/runner.py
+++ rajinipp/runner.py
@@ -6,12 +6,13 @@ to python scripts.
 
 import os
 import sys
+import warnings
 from pathlib import Path
 
 from loguru import logger
 
 from .lexer import Lexer
-from .parser.parser import Parser
+from .parser.parser import LineParser, ParserBase, ProgramParser
 from .utils import read_yml
 
 
@@ -21,8 +22,28 @@ class RppRunner:
     """
 
     def __init__(self) -> None:
+
         yml_path = os.path.join(Path(__file__).parent, "token.yml")
         self.tokens_dict = read_yml(yml_path)
+
+        self.__pgm_parser = self.__init_parser(ProgramParser)
+
+        # * we ignore warnings related to unused tokens.
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            logger.warning(
+                "LineParser supports only a subset of rajini++ features."
+            )
+            self.__line_parser = self.__init_parser(LineParser)
+
+    def __init_parser(self, Parser: ParserBase):
+        parser = Parser(list(self.tokens_dict.keys()))
+        parser.parse()
+        return parser.get_parser()
+
+    def __eval(self, parser, code):
+        parsed_tokens = parser.parse(self.tokenize(code))
+        return parsed_tokens.eval()
 
     def tokenize(self, code: str):
         """
@@ -38,7 +59,7 @@ class RppRunner:
         tokens = lexer.lex(code)
         return tokens
 
-    def exec(self, code: str, log_level: str = "ERROR"):
+    def exec(self, code: str, log_level: str = "INFO"):
         """
         exec executes a copmplete rajini++ program.
 
@@ -48,12 +69,20 @@ class RppRunner:
         """
         logger.remove()
         logger.add(sys.stderr, level=log_level)
+        self.__eval(self.__pgm_parser, code)
 
-        parser = Parser(list(self.tokens_dict.keys()))
-        parser.parse()
+    def eval(self, code_line: str):
+        """
+        eval evaluates a rajini++ statement.
 
-        parser = parser.get_parser()
+        **NOTE**: eval supports only a subset of rajini++ features.
 
-        parsed_tokens = parser.parse(self.tokenize(code))
+        Args:
+            code_line (str): rajini++ statement to evaluate.
 
-        parsed_tokens.eval()
+        Returns:
+            _type_: output of the evaluation.
+        """
+        logger.remove()
+        logger.add(sys.stderr, level="ERROR")
+        return self.__eval(self.__line_parser, code_line)
