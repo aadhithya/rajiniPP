e59393146dcd61578bc4e91345555276cf191102
Aadhithya Sankar <a.sankar@tum.de>
2022-05-16 01:58:26 +0200

    feat(flow_parser.py): add support for while loops and break statement

--- rajinipp/parser/flow_parser.py
+++ rajinipp/parser/flow_parser.py
@@ -1,6 +1,10 @@
-import pdb
-
-from ..ast.control import ForLoop, IfCondition, IfElseCondition
+from ..ast.control import (
+    Break,
+    ForLoop,
+    IfCondition,
+    IfElseCondition,
+    WhileLoop,
+)
 
 
 class ConditionalParser:
@@ -25,9 +29,19 @@ class LoopParser:
     """parse loops"""
 
     def parse(self, pg):
+        @pg.production("statement : BREAK_LOOP SEMI_COLON")
+        def break_stmt(p):
+            return Break()
+
         @pg.production(
             "statement : FOR_START forvar FOR_RANGE_START forvar FOR_RANGE_END"
             + " L_BRACE statements R_BRACE END_BLOCK SEMI_COLON"
         )
         def for_loop(p):
             return ForLoop(p[1], p[3], p[6])
+
+        @pg.production(
+            "statement : WHILE_LOOP expression L_BRACE statements R_BRACE END_BLOCK SEMI_COLON"
+        )
+        def while_loop(p):
+            return WhileLoop(p[1], p[3])
