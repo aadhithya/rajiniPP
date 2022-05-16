b5c54fcd7161ba8f3da09263567ba3423a1b2228
Aadhithya Sankar <a.sankar@tum.de>
2022-05-17 01:08:23 +0200

    refactor(parser): disable logging

--- rajinipp/parser/stmt_parser.py
+++ rajinipp/parser/stmt_parser.py
@@ -1,4 +1,4 @@
-from loguru import logger
+# from loguru import logger
 
 from ..ast.base import Print
 from ..ast.blocks import StatementsBlock
@@ -10,16 +10,16 @@ class StatementParser:
     def parse(self, pg):
         @pg.production("statements : statements statement")
         def statements(p):
-            logger.debug("Parser --> statements")
+            # logger.debug("Parser --> statements")
             return StatementsBlock(p[0], p[1])
 
         @pg.production("statements : ")
         def statements_empty(p):
-            logger.debug("Parser --> statements_empty")
-            logger.debug(p)
+            # logger.debug("Parser --> statements_empty")
+            # logger.debug(p)
             return StatementsBlock()
 
         @pg.production("statement : PRINT printexprs SEMI_COLON")
         def statement_print(p):
-            logger.debug("Parser --> statement_print")
+            # logger.debug("Parser --> statement_print")
             return Print(p[1])
