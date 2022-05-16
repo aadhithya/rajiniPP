e90cce9edd2ae0760d0b328ee1fee0d752bb1e80
Aadhithya Sankar <a.sankar@tum.de>
2022-05-17 01:14:01 +0200

    feat(ReturnException): add exception to handle return statements

--- rajinipp/execptions.py
+++ rajinipp/execptions.py
@@ -1,2 +1,8 @@
 class BreakException(Exception):
     pass
+
+
+class ReturnException(Exception):
+    def __init__(self, message, return_value) -> None:
+        super().__init__(message)
+        self.return_value = return_value
