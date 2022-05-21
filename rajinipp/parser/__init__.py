1e0bafe99bff5d1c7a1789dcc8d1e4b864763879
Aadhithya Sankar <a.sankar@tum.de>
2022-05-21 12:32:59 +0200

    fix: add init files to submodules

---
+++ rajinipp/parser/__init__.py
@@ -0,0 +1,23 @@
+from . import (
+    atom_parser,
+    expr_parser,
+    flow_parser,
+    func_parser,
+    ops_parser,
+    parser,
+    print_parser,
+    stmt_parser,
+    var_parser,
+)
+
+__all__ = [
+    "atom_parser",
+    "expr_parser",
+    "flow_parser",
+    "func_parser",
+    "ops_parser",
+    "parser",
+    "print_parser",
+    "var_parser",
+    "stmt_parser",
+]
